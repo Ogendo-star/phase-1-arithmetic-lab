8cc500bb0e5fa361b1afa65c78d992b442edab76
Masela Ogendo <puroselah@gmail.com>
2025-03-20 07:21:01 +0300

    feat:perfomed an arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+// Define num1 and num2 for multiplication
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
+
+// Generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1; 
+// Define num3 and num4 for modulus operation
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4; 
+
+// Find the highest number in a set
+let max = Math.max(10, 15, 20, 5);
